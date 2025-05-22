85a92bb4b865fd24efee64cc67fdf8decb6857b1
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-05-22 12:42:55 +0200

    full data csv with alla labels

--- Preprocessing/preprocess_data_mm_used.py
+++ Preprocessing/preprocess_data_mm_used.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def fallacy_to_category():
-    fallacies = pd.read_csv("data/afc_data_processed.csv") 
+    fallacies = pd.read_csv("../data/afc_data_processed.csv") 
 
     # Mappings for fallacy types to categories
     fallacy_to_category = {
@@ -19,6 +19,24 @@ def fallacy_to_category():
 
     
     fallacies['category'] = fallacies['label'].map(fallacy_to_category)
-    fallacies[['snippet', 'category']].to_csv("data/category_data_processed.csv", index=False)
+    fallacies[['snippet', 'category']].to_csv("../data/category_data_processed.csv", index=False)
 
-fallacy_to_category()
+
+
+def create_full_data_file():
+
+    fd = pd.read_csv("../data/afd_data_processed.csv")
+    fc = pd.read_csv("../data/afc_data_processed.csv")
+    f_cat = pd.read_csv("../data/category_data_processed.csv")
+
+    full_data = fd.copy()
+    full_data['fallacy_category'] = f_cat.iloc[:, 1]
+    full_data['fallacy_class'] = fc.iloc[:, 2]
+
+
+    full_data.rename(columns={full_data.columns[1]: 'fallacy_detection'}, inplace=True)
+    full_data.to_csv("../data/full_data_processed.csv", index=False)
+
+
+# fallacy_to_category()
+create_full_data_file()
