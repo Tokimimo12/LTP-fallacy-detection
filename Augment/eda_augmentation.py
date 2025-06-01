3ade7c5c62a1981c26f071e1c1d8b5d798e74ff9
xenia-d <91921195+xenia-d@users.noreply.github.com>
2025-06-01 15:21:12 +0200

    augmented balancing fallacy detection

--- Augment/eda_augmentation.py
+++ Augment/eda_augmentation.py
@@ -1,29 +1,45 @@
 from tqdm import tqdm
 import nltk
 import pandas as pd
+import os
+import sys
 
-from eda import eda
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from Augment.eda import eda
 
 nltk.download('wordnet')
 
-# Load data
-data = pd.read_csv("../data/MM_USED_fallacy/full_data_processed.csv")
-data_copy = data.copy()
+def eda_augmentation(data):
+    """
+    Perform EDA augmentation on the dataset.
+    This function reads the dataset, applies EDA techniques to augment sentences,
+    and saves the augmented data to a new CSV file.
+    """
 
-for index, row in tqdm(data.iterrows(), total=data.shape[0]):
-    sentence = row['snippet']
-    f_detect = row['fallacy_detection']
-    f_category = row['category']
-    f_class = row['class']
+    # Load data
+    # if not isinstance(data, pd.DataFrame):
+    #     data = pd.read_csv("../data/MM_USED_fallacy/full_data_processed.csv")
+    data_copy = data.copy()
 
-    if f_category == -1 or f_class == -1:
-        print(f"Skipping row {index} due to missing category or class.")
+    for index, row in tqdm(data.iterrows(), total=data.shape[0]):
+        sentence = row['snippet']
+        f_detect = row['fallacy_detection']
+        f_category = row['category']
+        f_class = row['class']
 
-    if len(sentence.split()) > 1:
-        aug_sentences = eda(sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=5)
+        if f_category == -1 or f_class == -1:
+            print(f"Skipping row {index} due to missing category or class.")
 
-        for aug_sentence in aug_sentences[:-1]: # Remove the last one since it is the original
-            new_row = [len(data_copy), aug_sentence, f_detect, f_category, f_class]
-            data_copy.loc[len(data_copy)] = new_row
+        if len(sentence.split()) > 1:
+            # only augment sentences that are fallacies (f_detect == 1)
+            if f_detect == 0:
+                continue
+            aug_sentences = eda(sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, p_rd=0.1, num_aug=10)
 
-data_copy.to_csv("../data/MM_USED_fallacy/aug_data.csv", index=False)
+            for aug_sentence in aug_sentences[:-1]: # Remove the last one since it is the original
+                new_row = [aug_sentence, f_detect, f_category, f_class]
+                data_copy.loc[len(data_copy)] = new_row
+
+    data_copy.to_csv("../data/MM_USED_fallacy/aug_data.csv", index=False)
+
+    return data_copy
