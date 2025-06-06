3d48d3e769c21b8884021b8023da18ff840ec4bb
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-06-06 23:47:17 +0200

    augment

--- Model/train.py
+++ Model/train.py
@@ -334,7 +334,7 @@ def train_model(bert_model_name = "DistilBert", mtl=True, augment=True, num_epoc
 if __name__ == "__main__":
     # Example usage
     mtl = False  # Set to False for single-task learning
-    augment = False  # Set to False if you don't want to use EDA augmentation
+    augment = True  # Set to False if you don't want to use EDA augmentation
     num_epochs = 200  # Adjust as needed
     batch_size = 32  # Adjust as needed
     htc=True
