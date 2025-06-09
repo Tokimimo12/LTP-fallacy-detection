2e74ac16532b832e90e78f238c67ad53f7fc8286
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-06-09 21:21:41 +0200

    idk

---
+++ prompting/hierarchical_prompting.py
@@ -0,0 +1,289 @@
+import json
+import random
+import logging
+from retry import retry
+from transformers import pipeline, AutoConfig
+
+from tenacity import retry, stop_after_attempt, wait_fixed
+import sys
+import pandas as pd
+import os
+from utils import get_possible_outputs
+
+import argparse
+
+random.seed(4)
+
+def prompt_zeroshot_hierarchical(text: str, level: str) -> str:
+    _, category_labels, class_labels = get_possible_outputs()
+    if level == 'detection':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 1. <Yes/No>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": "1. Is the text fallacious? Only answer with 'yes' or 'no'."},
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+    elif level == 'category':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 2. <Fallacy Category>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": "You previously indicated that the following test is fallacious."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": f"2. What category of fallacy is it? You only have to answer with one of the following labels: {category_labels}"},
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+    elif level == 'class':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 3. <Specific Type>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": "You previously indicated that the following test is a fallacy that falls under the category: {predicted_category}."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": f"3. What specific kind of fallacy is it? You only have to answer with one of the following labels: {class_labels}."}
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+
+    print(f"Generated prompt for {level} level:\n{prompt}\n")
+    return prompt
+
+def prompt_oneshot_hierarchical(text: str, level: str, predicted_category: str) -> str:
+    _, category_labels, class_labels = get_possible_outputs()
+    if level == 'detection':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 1. <Yes/No>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": "1. Is the text fallacious? Only answer with 'yes' or 'no'."},
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+    elif level == 'category':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 2. <Fallacy Category>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": "You previously indicated that the following test is fallacious."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": f"2. What category of fallacy is it? You only have to answer with one of the following labels: {category_labels}"},
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+    elif level == 'class':
+        messages = [
+            {"role": "system", "content": "Your task is to simply and promptly give bare answer the next question. The answer needs to be in the following format: 3. <Specific Type>. Do not generate anything beyond this line. Do not explain or continue after the first line."},
+            {"role": "user", "content": "You previously indicated that the following test is a fallacy that falls under the category: {predicted_category}."},
+            {"role": "user", "content": f"Text: {text}"},
+            {"role": "user", "content": f"3. What specific kind of fallacy is it? You only have to answer with one of the following labels: {class_labels}."}
+        ]
+
+        prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+        prompt += "\nAssistant:\n"  # hint model it's time to respond
+
+    print(f"Generated prompt for {level} level:\n{prompt}\n")
+    return prompt
+
+def prompt_oneshot(text:str) -> str:
+    # example from train set, sample 466
+    example = (
+        "We have to practice what we preach.\n"
+        "1. Yes\n"
+        "2. Fallacy of Emotion\n"
+        "3. Slogans\n"
+    )
+
+    _, category_labels, class_labels = get_possible_outputs()
+
+    messages = [
+        {   "role": "system",
+            "content": "Your task is to simply and promptly give bare answer the next 3 questions. The answer needs to be in the following format, each on a new line: \n1. <Yes/No>\n2. <Fallacy Category>\n3. <Specific Type>.\n Do not generate anything beyond these three lines. Do not explain or continue after the third line."
+        },
+        {"role": "user", "content": "Below is one example of how to answer the task:"},
+        {"role": "user", "content": example},
+        {"role": "user", "content": "Now answer the following statement:"},
+        {"role": "user", "content": "Now it's your turn:"},
+        {"role": "user", "content": f"Text: {text}"},
+        {"role": "user", "content": "1. Is the text fallacious? Only answer with 'yes' or 'no'."},
+        {"role": "user", "content": f"2. What category of fallacy is it? You only have to answer with one of the following labels: {category_labels}, or ['None'] if it is not fallacious."},
+        {"role": "user", "content": f"3. What specific kind of fallacy is it? You only have to answer with one of the following labels or other labels you see fit: {class_labels}, or ['None'] if it is not fallacious."},
+    ]
+
+    prompt = "\n".join([f"{m['role']}: {m['content']}" for m in messages])
+    prompt += "\nAssistant:\n"  # hint model it's time to respond
+
+    return prompt
+
+def extract_answers(answer: str) -> tuple:
+    lines = answer.strip().splitlines()
+
+    try:
+        # Find the index of the assistant's response and save the 3 answers right after it
+        assistant_index = next(i for i, line in enumerate(lines) if line.strip().lower().startswith("assistant:"))
+        next_lines = lines[assistant_index + 1 : assistant_index + 4]
+        if len(next_lines) < 3:
+            raise ValueError("Not enough lines returned by model.")
+        fallacious, category, specific_type = [line.strip() for line in next_lines]
+        return fallacious, category, specific_type
+    except (StopIteration, IndexError, ValueError):
+        # otherwise just save the last 3 lines of your response
+        lines = answer.strip().splitlines()[-3:]  # last 3 lines expected
+        fallacious = lines[0].strip()
+        category = lines[1].strip()
+        specific_type = lines[2].strip()
+    return fallacious, category, specific_type
+
+def extract_single_answer(answer: str) -> tuple:
+    lines = answer.strip().splitlines()
+
+    try:
+        # Find the index of the assistant's response and save the 3 answers right after it
+        assistant_index = next(i for i, line in enumerate(lines) if line.strip().lower().startswith("assistant:"))
+        next_lines = lines[assistant_index + 1 : assistant_index + 2]
+        if len(next_lines) < 1:
+            raise ValueError("Not enough lines returned by model.")
+        fallacious = next_lines[0].strip()
+        return fallacious
+    except (StopIteration, IndexError, ValueError):
+        # otherwise just save the last line of your response
+        lines = answer.strip().splitlines()[-1:]  # last line expected
+        fallacious = lines[0].strip()
+    return fallacious
+
+def process_statements(statements: list, generator, mode) -> list:
+    results = []
+    for text in statements:
+        for level in ['detection', 'category', 'class']:
+            print(mode)
+            if mode == "zero-shot":
+                if level == 'detection':
+                    print(f"##### Processing text: {text} at level: {level}")
+                    prompt = prompt_zeroshot_hierarchical(text, level)
+                    output = generator(prompt, max_new_tokens=128)[0]["generated_text"]
+                    print("output\n", output)
+                    fallacious = extract_single_answer(output)
+                    print(f"Fallacious: {fallacious}")
+
+                    results.append({
+                        "input_statement": text,
+                        "fallacious": fallacious,
+                        "mode": mode,
+                    })
+
+                    if fallacious.lower() == 'no':
+                        break
+                elif level == 'category':
+                    print(f"##### Processing text: {text} at level: {level}")
+                    prompt = prompt_zeroshot_hierarchical(text, level)
+                    output = generator(prompt, max_new_tokens=128)[0]["generated_text"]
+                    print("output\n", output)
+                    category = extract_single_answer(output)
+                    print(f"Category: {category}")
+
+                    results[-1]['category'] = category
+                elif level == 'class':
+                    print(f"##### Processing text: {text} at level: {level}")
+                    predicted_category = results[-1]['category']
+                    prompt = prompt_zeroshot_hierarchical(text, level).format(predicted_category=predicted_category)
+                    output = generator(prompt, max_new_tokens=128)[0]["generated_text"]
+                    print("output\n", output)
+                    specific_type = extract_single_answer(output)
+                    print(f"Specific Type: {specific_type}")
+
+                    results[-1]['specific_type'] = specific_type
+            elif mode == "one-shot":
+                pass
+            else:
+                raise ValueError("Mode must be either 'zero-shot' or 'one-shot'.")
+
+            
+    return results
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process statements with a language model.")
+    parser.add_argument("--mode", type=str, choices=["zero-shot", "one-shot"], default="zero-shot", help="Mode of prompting: zero-shot or one-shot.")
+    parser.add_argument("--model", type=str, default="mistralai", help="Model to use for generation.")
+    args = parser.parse_args()
+    MODE = args.mode
+    print(f"Running in {MODE} mode.")
+
+    #GENERATiION CONFIGURATION
+    generation_models = {
+    "phi-4": "microsoft/phi-4",#apparently is a text generation model and does not support question-answering tasks  
+    "menda": "weathermanj/Menda-3b-Optim-200" ,
+    "mistralai": "mistralai/Mistral-7B-v0.1",
+    "tinyllama" : "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    "llama": "meta-llama/Llama-3.2-3B", #apparently you need token for this, to do later
+    "llama-instruct": "meta-llama/Llama-3.2-3B-Instruct"
+    # "llama-2": "meta-llama/Llama-2-7b"
+    }
+
+    data = pd.read_csv("../data/MM_USED_fallacy/splits/test_data.csv")
+
+    indices = []
+    statements = []
+    pred_detection = []
+    pred_categories = []
+    pred_classes = []
+    gt_detection = []
+    gt_categories = []
+    gt_classes = []
+    counter = 0
+    
+    model_name = generation_models[args.model]
+    print(f"Using model: {model_name}")
+
+    try:
+        config = AutoConfig.from_pretrained("meta-llama/Llama-3.2-3B")
+        print("Access successful!")
+    except Exception as e:
+        print(f"Still having issues: {e}")
+
+
+    generator = pipeline(
+        "text-generation",
+        model=model_name,
+        model_kwargs={"torch_dtype": "auto"},
+        device_map="auto",
+    )
+    #print which device the model is on
+    print(f"Model is loaded on device: {generator.device}")
+
+    # loop through the data
+    for index, row in data.iterrows():
+        if index > 2:
+            break
+        print(f"Processing row {index + 1}/{len(data)}: {row['ID']}")
+        index_ID = row['ID']
+        counter += 1
+
+        indices.append(index_ID)
+        statements.append(row['snippet'])
+        gt_detection.append(row['fallacy_detection'])
+        gt_categories.append(row['category'])
+        gt_classes.append(row['class'])
+
+        snippet = row['snippet']
+        detection = row['fallacy_detection']
+        category = row['category']
+        specific_type = row['class']
+
+        answer = process_statements([snippet], generator, MODE)
+
+        pred_detection.append(answer[0]['fallacious'])
+        pred_categories.append(answer[0]['category'])
+        pred_classes.append(answer[0]['specific_type'])
+
+    # Save the results to a CSV file
+    results_df = pd.DataFrame({
+        'index': indices,
+        'statement': statements,
+        'pred_detection': pred_detection,
+        'pred_categories': pred_categories,
+        'pred_classes': pred_classes,
+        'gt_detection': gt_detection,
+        'gt_categories': gt_categories,
+        'gt_classes': gt_classes
+    })	    
+    results_df.to_csv(os.path.join('results', f"hierarchical_{MODE}_{args.model}.csv"), index=False)
+    
