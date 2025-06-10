82b7d08b7b4aa13ae8d3102729d61da9ecfe1f49
VojiWest <131270168+VojiWest@users.noreply.github.com>
2025-06-11 01:47:03 +0200

    clean up

--- Evaluation/HierarchicalEvaluator.py
+++ Evaluation/HierarchicalEvaluator.py
@@ -168,7 +168,8 @@ class HierarchicalEvaluator:
         if self.head_type == "MTL 6":
             self.avg_class_and_detection_f1 = np.mean(np.append(class_[1][:6, 3], detection[3]))
         elif self.head_type == "STL":
-            self.avg_class_and_detection_f1 = np.mean((class_[1][3]))
+            _, _, rev_f1 = self.compute_reversed_f1_for_class_6()
+            self.avg_class_and_detection_f1 = np.mean(np.append(class_[1][:6, 3], rev_f1))
 
         return{ 
             "detection": {
