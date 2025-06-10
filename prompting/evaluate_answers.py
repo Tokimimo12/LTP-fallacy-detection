8857acebdbeb7cca54d0fba1b211164462072b23
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-06-10 10:45:53 +0200

    updated to deal with tensors as strings...

--- prompting/evaluate_answers.py
+++ prompting/evaluate_answers.py
@@ -7,37 +7,76 @@ from Evaluation.HierarchicalEvaluator import HierarchicalEvaluator
 import json
 
 def post_process_pred(pred, conver_dict, level="fallacy"):
-    splits = pred.split('. ')
-    answer = splits[-1:]
-    answer = [a.lower() for a in answer]
-    answer = answer[0]
-    if level == "fallacy":
-        if answer == "yes":
-            return 1
-        elif answer == "no":
-            return 0
+    # check if pred is a string
+
+    if type(pred) == str:
+
+        splits = pred.split('. ')
+        answer = splits[-1:]
+        answer = [a.lower() for a in answer]
+        answer = answer[0]
+        if level == "fallacy":
+            if answer == "yes":
+                return 1
+            elif answer == "no":
+                return 0
+            else:
+                return -1
+        elif level == "category":
+            try:
+                return conver_dict[answer]
+            except KeyError:
+                # print(f'Unknown category: {answer}')
+                return -1
+        elif level == "class":
+            try:
+                return conver_dict[answer]
+            except KeyError:
+                # print(f'Unknown class: {answer}')
+                return -1
+    else:
+        # print(f"Unexpected type for pred: {type(pred)}. Expected str.")
+        # print(f"Pred value: {pred}")
+        return -1
+
+def convert_tensors_to_ints(tensor):
+    #  remove "tensor()" and convert to int
+    if isinstance(tensor, str):
+        tensor = tensor.replace("tensor(", "").replace(")", "")
+    tensor = int(tensor)
+    return tensor
+
+def get_gt_data(gt_data, IDs):
+    gt_data = gt_data.set_index('ID')
+    detection_gt = []
+    category_gt = []
+    classify_gt = []
+
+    for ID in IDs:
+        if ID in gt_data.index:
+            detection_gt.append(gt_data.at[ID, 'fallacy_detection'])
+            category_gt.append(gt_data.at[ID, 'category'])
+            classify_gt.append(gt_data.at[ID, 'class'])
         else:
-            return -1
-    elif level == "category":
-        try:
-            return conver_dict[answer]
-        except KeyError:
-            print(f'Unknown category: {answer}')
-            return -1
-    elif level == "class":
-        try:
-            return conver_dict[answer]
-        except KeyError:
-            print(f'Unknown class: {answer}')
-            return -1
+            # print(f"ID {ID} not found in ground truth data.")
+            detection_gt.append(-1)
+            category_gt.append(-1)
+            classify_gt.append(-1)
+
+    return detection_gt, category_gt, classify_gt
+
 
 def get_processed_df(data):
     ntclass, ntcat, ntfallacy = get_reverse_dicts()
     processed_df = data.copy()
+
+    processed_df['index'] = [convert_tensors_to_ints(x) for x in processed_df['index']]
+
     for index, row in data.iterrows():
         detection = row['pred_detection']
         category = row['pred_categories']
         spec_class = row['pred_classes']
+        # print(f"Processing row {index}: {type(detection)}, {type(category)}, {type(spec_class)}")
         det_pred = post_process_pred(detection, ntfallacy, level="fallacy")
         cat_pred = post_process_pred(category, ntcat, level="category")
         class_pred = post_process_pred(spec_class, ntclass, level="class")
@@ -46,13 +85,11 @@ def get_processed_df(data):
         processed_df.at[index, 'pred_detection'] = det_pred
         processed_df.at[index, 'pred_categories'] = cat_pred
         processed_df.at[index, 'pred_classes'] = class_pred
-    
-    processed_df['gt_detection'].astype(int)
 
     return processed_df
 
 if __name__ == "__main__":
-    results_dir = "results"
+    results_dir = "results/zero-shot_simple"
 
     for filename in os.listdir(results_dir):
         if filename.endswith(".csv"):
@@ -60,6 +97,7 @@ if __name__ == "__main__":
             print(f"Evaluating Model: {model_name}")
             file_path = os.path.join(results_dir, filename)
             data = pd.read_csv(file_path)
+            gt_data = pd.read_csv("../data/MM_USED_fallacy/splits/test_data.csv")
 
             # Process the DataFrame
             processed_df = get_processed_df(data)
@@ -67,17 +105,17 @@ if __name__ == "__main__":
             detection_preds = processed_df['pred_detection'].tolist()
             group_preds = processed_df['pred_categories'].tolist()
             classify_preds = processed_df['pred_classes'].tolist()
-            detection_gt = processed_df['gt_detection'].tolist()
-            group_gt = processed_df['gt_categories'].tolist()
-            classify_gt = processed_df['gt_classes'].tolist()
+            IDs = processed_df['index'].tolist()
+
+            detection_gt, category_gt, classify_gt = get_gt_data(gt_data, IDs)
 
             evaluator = HierarchicalEvaluator(num_classes=7, head_type='STL')
 
-            print(detection_preds)
-            print(detection_gt)
+            # print(detection_preds)
+            # print(detection_gt)
 
             # Add predictions and labels to evaluator
-            for det_p, grp_p, cls_p, det_g, grp_g, cls_g in zip(detection_preds, group_preds, classify_preds, detection_gt, group_gt, classify_gt):
+            for det_p, grp_p, cls_p, det_g, grp_g, cls_g in zip(detection_preds, group_preds, classify_preds, detection_gt, category_gt, classify_gt):
                 evaluator.add(
                     predictions=(det_p, grp_p, cls_p),
                     ground_truth=(det_g, grp_g, cls_g)
