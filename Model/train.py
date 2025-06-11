3c44fa7a0706b98f75de50a0a13137010b60fa14
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-06-11 13:48:06 +0200

    increased batch size

--- Model/train.py
+++ Model/train.py
@@ -323,7 +323,7 @@ if __name__ == "__main__":
     head_type = "MTL 6"  # Set to "STL" for single task, "MTL 2" for 2 classes in final layer, and "HTC" for hierarchical softmax
     augment = "LLM"  # Set to "None" for no augmentation, "EDA" for EDA augmentation or "LLM" for LLM generated augmentation, "LLM+EDA" for both, and "Undersample" to undersample non-fallacy
     num_epochs = 20  # Adjust as needed
-    batch_size = 1  # Adjust as needed
+    batch_size = 32  # Adjust as needed
 
     for head_type in ["HTC"]:
         for augment in ["LLM"]:
